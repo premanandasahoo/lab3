52b9609a07dd32cd6c1e8884511e97a43c4821df
premanandasahoo <42239894+premanandasahoo@users.noreply.github.com>
2018-08-17 09:23:16 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+int sum = 5+7;
+cout <<sum<<endl;
+cout <<sizeof(sum)<<endl;
+char a= 74;
+cout <<sizeof(a)<<endl;
+bool b= false;
+cout <<sizeof(b)<<endl;
+float u= 7.44
+cout <<sizeof(u)<<endl;
+double d= 88.99
+cout <<sizeof(d)<<endl;
+}
+
---
+++ lab3_q2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int sum = 9+4;
+cout <<sum<<endl;
+int difference = 8-7;
+cout <<difference<<endl;
+int multiply= 9*4;
+cout <<multiply<<endl;
+int divide=9/6;
+cout <<divide<<endl;
+int modulus= 8%3;
+cout <<modulus<<endl;
+}
